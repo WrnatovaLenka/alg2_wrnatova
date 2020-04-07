519eeaa1a434797afce0b2db7f70f699b0616e91
Lenka_Wrnatova <lenka.wrnatova@red-fox.cz>
2020-04-07 08:56:12 +0200

    created bank project

---
+++ Bank/src/bank/Bank.java
@@ -0,0 +1,16 @@
+package bank;
+
+/**
+ *
+ * @author lenka.wrnatova
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
